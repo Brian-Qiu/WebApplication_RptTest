11068c3cd29f94aa19a3438301b00cfd83b68a1d
Brian <Brian@BRIANQIU>
2017-05-27 09:08:38 +0800

    添加项目文件。

---
+++ WebApplication_RptTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_RptTest.Startup))]
+namespace WebApplication_RptTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
